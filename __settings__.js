d575a5c5e551bd73d5915c39761d48dd89ea0b46
Kyosuke-Fukui <fs72os.kyosuke@gmail.com>
2021-04-23 23:30:11 +0900

    initial commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1137573.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 46466413, 46467648, 46468493, 46470074, 46470680 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/46466536/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/46466537/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/46466535/1/ammo.js', 'preload' : true},
+];
